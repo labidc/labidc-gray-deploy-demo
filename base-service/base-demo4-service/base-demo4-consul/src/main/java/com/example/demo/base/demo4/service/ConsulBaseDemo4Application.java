07cf6872b73e4af661090e4cc69c610cdba5dae8
熊闯 <xiongchuang@iwubida.com>
2019-01-04 16:06:16 +0800

    适应 1.0.5

--- base-service/base-demo4-service/base-demo4-consul/src/main/java/com/example/demo/base/demo4/service/ConsulBaseDemo4Application.java
+++ base-service/base-demo4-service/base-demo4-consul/src/main/java/com/example/demo/base/demo4/service/ConsulBaseDemo4Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServiceDemo4Application {
+public class ConsulBaseDemo4Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServiceDemo4Application.class, args);
+        SpringApplication.run(ConsulBaseDemo4Application.class, args);
     }
 
 }
